e43bf48ff659653df19a990209b18f68ebce4e73
AnshSinha9140 <56307831+AnshSinha9140@users.noreply.github.com>
2022-09-25 13:27:57 +0530

    Delete Constants.kt

--- app/src/main/java/com/example/notesapp/utils/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.notesapp.utils
-
-object Constants {
-    const val TAG = "MYANSHCODES"
-    const val BASE_URL = "https://notesappservices.herokuapp.com/"
-    const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
-    const val USER_TOKEN = "USER_TOKEN"
-}
